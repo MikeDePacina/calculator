8558f5bf285b96a3d43fefaa8083b4e878c77a35
Mike De Pacina <mikedpacina@gmail.com>
2021-10-02 18:52:57 -0700

    modified js

--- file.js
+++ file.js
@@ -1,10 +1,79 @@
-const field = document.getElementByClassName('container')
-Array.from(document.getElementByClassName('container'))
-field.forEach(element => {
-    addEventListener('click', display())
-});
+const numButtons = document.querySelectorAll('[data-number]')
+const opButtons = document.querySelectorAll('[data-operation]')
+const clearButton = document.querySelector('[data-clear]')
+const delButton = document.querySelector('[data-delete]')
+const ansButton = document.querySelector('[data-ans]')
+const currentTextElement = document.querySelector('[data-curr')
+const prevTextElement = document.querySelector('[data-prev]')
+const equalsButton = document.querySelector('[data-equals]')
 
-function display(){
-    const output = document.getElementById('field')
-    output.innerHTML = 1
+class Calculator{
+    constructor(currentTextElement, prevTextElement){
+        this.currentTextElement = currentTextElement
+        this.prevTextElement = prevTextElement
+        this.clear()
+    }
+
+    clear(){
+        this.currentTextElement = ''
+        this.prevTextElement = ''
+    }
+
+    delete(){
+        this.currentTextElement = this.currentTextElement.toString().slice(0,-1)
+    }
+
+    updateDisplay(){
+        currentTextElement.innerText = this.currentTextElement
+        prevTextElement.innerText = this.prevTextElement
+    }
+
+    compute(){
+
+    }
+
+    appendNumber(number){
+        if(number === '.' && this.currentTextElement.includes('.')) return
+        this.currentTextElement = this.currentTextElement.toString() + number.toString()
+    }
+
+    chosenOperation(operation){
+        if(currentTextElement === '') return
+        this.operation = operation
+        this.prevTextElement = currentTextElement
+        this.currentTextElement = ''
+
+    }
+
 }
+
+const calc = new Calculator(currentTextElement, prevTextElement)
+
+numButtons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        calc.appendNumber(button.innerText)
+        calc.updateDisplay()
+    })
+})
+
+opButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        calc.chosenOperation(button.innerText)
+        calc.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', ()=>{
+    calc.compute()
+    calc.updateDisplay()
+})
+
+clearButton.addEventListener('click', ()=>{
+    calc.clear()
+    calc.updateDisplay()
+})
+
+delButton.addEventListener('click', ()=>{
+    calc.delete()
+    calc.updateDisplay()
+})
